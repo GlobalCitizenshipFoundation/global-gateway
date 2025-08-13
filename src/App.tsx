bebde3b79a949d1effcabda5fb363c354067be8a
[dyad] <aaryan@globalcitizenshipfoundation.org>
2025-08-13 04:01:26 +0200

    [dyad] wrote 1 file(s), deleted 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -35,7 +34,7 @@ const App = () => (
       disableTransitionOnChange
     >
       <TooltipProvider>
-        <Toaster />
+        {/* Removed <Toaster /> as sonner is used */}
         <Sonner />
         <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <SessionContextProvider>
