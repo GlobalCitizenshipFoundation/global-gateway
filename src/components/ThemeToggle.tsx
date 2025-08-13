5cf271b2c877c496f0c2c9b4cc31d71ac9f8c063
[dyad] <aaryan@globalcitizenshipfoundation.org>
2025-08-13 02:24:57 +0200

    [dyad] Fixed the theme toggle button. - wrote 1 file(s)

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -19,12 +19,9 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          {/* Wrap multiple icons/span in a single span to satisfy React.Children.only requirement */}
-          <span className="relative"> {/* Add relative positioning to the parent span */}
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 absolute" /> {/* Add absolute positioning */}
-            <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 absolute" /> {/* Add absolute positioning */}
-            <span className="sr-only">Toggle theme</span>
-          </span>
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
