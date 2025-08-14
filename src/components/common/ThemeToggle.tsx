8076b6b66bc4a9eea2651d87c9c898ec68f58633
[dyad] <aaryan@globalcitizenshipfoundation.org>
2025-08-14 12:42:41 +0200

    [dyad] wrote 1 file(s)

--- src/components/common/ThemeToggle.tsx
+++ src/components/common/ThemeToggle.tsx
@@ -18,10 +18,13 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon"> {/* Use Button component directly */}
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 absolute" />
-          <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 absolute" />
-          <span className="sr-only">Toggle theme</span>
+        <Button variant="ghost" size="icon">
+          {/* Wrap all children in a single span to satisfy React.Children.only */}
+          <span>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 absolute" />
+            <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 absolute" />
+            <span className="sr-only">Toggle theme</span>
+          </span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
