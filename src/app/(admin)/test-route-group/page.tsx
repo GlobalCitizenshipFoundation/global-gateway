39232de8a8c711655855ea5aaf0f7ea57dee7873
[dyad] <aaryan@globalcitizenshipfoundation.org>
2025-08-29 20:14:20 +0200

    [dyad] Creating a minimal test page in the admin route group to diagnose 404s - wrote 1 file(s)

---
+++ src/app/(admin)/test-route-group/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TestRouteGroupPage() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Admin Test Page Works!</h1>
+      <p>If you see this, the (admin) route group is working.</p>
+    </div>
+  );
+}
