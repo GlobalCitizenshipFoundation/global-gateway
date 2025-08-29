9416fc6794e5125c8d2a806a6e5625c56452a306
[dyad] <aaryan@globalcitizenshipfoundation.org>
2025-08-29 13:11:32 +0200

    [dyad] Constraining desktop layout to viewport height for independent scrolling - wrote 1 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen h-full`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-screen`}
       >
         <ThemeProvider
           attribute="class"
