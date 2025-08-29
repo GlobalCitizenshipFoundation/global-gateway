5777b6805eb6463240d0b2f005823170b0ab1cfc
[dyad] <aaryan@globalcitizenshipfoundation.org>
2025-08-30 01:59:14 +0200

    [dyad] Verifying file paths and testing a simple root-level page to diagnose persistent routing issues - wrote 1 file(s)

---
+++ src/app/simple-test-page/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SimpleTestPage() {
+  return (
+    <div className="p-4 text-center text-foreground bg-background">
+      <h1>Simple Test Page Works!</h1>
+      <p>If you see this, basic routing is functional.</p>
+    </div>
+  );
+}
